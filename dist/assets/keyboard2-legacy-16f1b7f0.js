27e39aaa513f3da8720ecf41a0a8d54381e2ac70
SergioAndrade22 <sergio.david2201@gmail.com>
2023-08-06 22:37:48 -0300

    Adds latest build

--- dist/assets/keyboard2-legacy-16f1b7f0.js
+++ dist/assets/keyboard2-legacy-16f1b7f0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ab5cddd2.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-9af74bdb.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dist/assets/swipe-back-legacy-eeb54e63.js
+++ dist/assets/swipe-back-legacy-eeb54e63.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ab5cddd2.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-9af74bdb.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
